8897aae3fa6a3ef1ee5bdbfb5a4c97c23384ce8d
Braydon Dudley <bdudley2@illinois.edu>
2021-03-04 11:26:28 -0600

    Very basic drawable canvas
    starting point prototype. allows us to draw on the UI canvas using WASD. (Needs optimized very badly to avoid instantiating too many objects)

---
+++ FactionLocked/Assets/draw.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class draw : MonoBehaviour
+{
+
+    public GameObject square;
+    public GameObject circle;
+    float speed = 0.05f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dx = Input.GetAxis("Horizontal") * speed;
+        float dy = Input.GetAxis("Vertical") * speed;
+        circle.transform.Translate(dx, dy, 0.0f);
+        if (dx != 0.0f || dy != 0.0f) {
+            Instantiate(square.transform, circle.transform.position, circle.transform.rotation);
+        }
+    }
+}
