a6666d13b614f92a2b29de4b1e43f0c31f021b9f
tessarm2 <tessarm2@illinois.edu>
2021-03-13 15:12:14 -0600

    Add drawable canvas

---
+++ FactionLocked/Assets/draw.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class draw : MonoBehaviour
+{
+
+    public GameObject square;
+    public GameObject circle;
+    public GameObject PathParent;
+    float speed = 0.05f;
+    private Vector2 screenBounds;
+    float dx;
+    float dy;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         dx = Input.GetAxis("Horizontal") * speed;
+         dy = Input.GetAxis("Vertical") * speed;
+        circle.transform.Translate(dx, dy, 0.0f);
+        Vector3 clampedPos = circle.transform.position;
+        clampedPos.x = Mathf.Clamp(clampedPos.x, -1.13f, 1.13f);
+        clampedPos.y = Mathf.Clamp(clampedPos.y, 2.25f, 3.35f);
+        circle.transform.position = clampedPos;
+        if (dx != 0.0f || dy != 0.0f)
+        {
+            Transform squarePart = Instantiate(square.transform, circle.transform.position, circle.transform.rotation);
+            squarePart.SetParent(PathParent.transform);
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "body")
+        {
+            dx = 0;
+            dy = 0;
+        }
+    }
+}
