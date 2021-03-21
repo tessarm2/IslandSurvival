4e1b95fe504e4f31e4eafbc4ed2daed0e264f5f5
Braydon Dudley <bdudley2@illinois.edu>
2021-03-21 18:02:39 -0500

    Initial Progress Dialogue Interaction

--- FactionLocked/Assets/Scripts/Braydon/Dialogue.cs
+++ FactionLocked/Assets/Scripts/Braydon/Dialogue.cs
@@ -10,33 +10,45 @@ public class Dialogue : MonoBehaviour
     public TextMeshProUGUI textDisplay;
     public TextMeshProUGUI nextDisplay;
     public string[] sentences;
-    public float textWaitSpeed = 0.02f;
+    public float textWaitTime = 0.02f;
+
+    Coroutine currRoutine;
+
 
 
     private int index = 0;
     private bool isTextMoving = false;
     private float speedMult = 1f;
 
+    public void ResetDialogue() {
+        StopCoroutine(currRoutine);
+        textDisplay.text = "";
+        index = 0; // starting index for character's dialogue
+        currRoutine = StartCoroutine(TypingRoutine());
+    }
+
     private void Start() {
         textDisplay.text = "";
-        StartCoroutine(TypingRoutine());
+        index = 0;
+        currRoutine = StartCoroutine(TypingRoutine());
     }
 
     IEnumerator TypingRoutine() {
 
         if (nextDisplay) nextDisplay.text = "";
         isTextMoving = true;
+        speedMult = 1f;
 
-        yield return new WaitForSeconds(textWaitSpeed * speedMult * 20f);
+        yield return new WaitForSeconds(textWaitTime * speedMult * 5f);
 
         // displays each letter in a sentences at some display speed
         foreach (var letter in sentences[index].ToCharArray())
         {
             textDisplay.text += letter;
-            yield return new WaitForSeconds(textWaitSpeed * speedMult);
+            yield return new WaitForSeconds(textWaitTime * speedMult);
         }
 
-        yield return new WaitForSeconds(textWaitSpeed * speedMult * 20f);
+        yield return new WaitForSeconds(textWaitTime * speedMult * 15f);
 
         isTextMoving = false;
         if (nextDisplay) nextDisplay.text = ">";
@@ -46,20 +58,33 @@ public class Dialogue : MonoBehaviour
         if (index < sentences.Length - 1) {
             index++;
             textDisplay.text = "";
-            StartCoroutine(TypingRoutine());
+            currRoutine = StartCoroutine(TypingRoutine());
         } else {
             textDisplay.text = "----  No more Text to Display  ----";
         }
     }
 
+    // private void onCanvasEnabled() {
+    //     textDisplay.text = "";
+    //     index = 0;
+    //     StartCoroutine(TypingRoutine());
+    // }
+
     private void Update() {
-        speedMult = 1f;
-        if (Input.GetKey("f")) {
-            if (Input.GetKeyDown("f") && !isTextMoving) {
+        if (Input.GetKeyDown("f")) {
+            if (!isTextMoving) {
                 NextSentence();
             } else {
-                speedMult = 0.01f;
+                toggleSpeedMult();
             }
+        }
+    }
+
+    private void toggleSpeedMult() {
+        if (speedMult == 0.01f) {
+            speedMult = 1f;
+        } else {
+            speedMult = 0.01f;
         }
     }
 }
---
+++ FactionLocked/Assets/Scripts/Braydon/InteractDialogue.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractDialogue : MonoBehaviour
+{
+
+    bool inRange = false;
+    bool outRange = false;
+    public Behaviour halo;
+    public GameObject dialogueCanvas;
+    public GameObject dialogueManager;
+    // public GameObject puzzleCam;
+    // public GameObject playerBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // playerBody.SetActive(true);
+        // puzzleCam.SetActive(false);
+        dialogueCanvas.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 3D player to dialogue
+        if (Input.GetKeyDown(KeyCode.E) && inRange && !dialogueCanvas.activeSelf)
+        {
+            // outRange = false;
+            SwapEnabled();
+            dialogueManager.GetComponent<Dialogue>().ResetDialogue();
+            Debug.Log("User interacted with a character. Brings up Dialogue Box");
+        }
+
+        // leave dialogue box
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            dialogueManager.GetComponent<Dialogue>().ResetDialogue();
+            SwapEnabled();
+            Debug.Log("User finished the interaction with the character");
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // if (other.GetType() == typeof(SphereCollider)) {
+            if(other.gameObject.tag == "Player")
+            {
+                inRange = true;
+                halo.enabled = true;
+            }
+        // }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        // if (other.GetType() == typeof(SphereCollider)) {
+            if (other.gameObject.tag == "Player")
+            {
+                inRange = false;
+                halo.enabled = false;
+            }
+        // }
+        // if (other.GetType() == typeof(BoxCollider)) {
+        //     if(other.gameObject.tag == "Player")
+        //     {
+        //         outRange = true;
+        //     }
+        // }
+        
+    }
+
+    void SwapEnabled() {
+        // playerBody.SetActive(!playerBody.activeSelf);
+        // puzzleCam.SetActive(!puzzleCam.activeSelf);
+        dialogueCanvas.SetActive(!dialogueCanvas.activeSelf);
+    }
+}
