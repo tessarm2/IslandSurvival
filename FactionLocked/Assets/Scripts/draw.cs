f57e95df2e93a3ed23d808d6769e889b73fcda63
tessarm2 <tessarm2@illinois.edu>
2021-03-22 16:53:14 -0500

    Trying to merge Brandon Canvas

---
+++ FactionLocked/Assets/Scripts/draw.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class draw : MonoBehaviour
+{
+
+    public GameObject square;
+    public GameObject circle;
+    public GameObject PathParent;
+    public float speed = 0.05f;
+    float dx;
+    float dy;
+    public Stack<int> directions;
+    private List<Transform> path;
+    private bool goingBackwards = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        path = new List<Transform>();
+        directions = new Stack<int>();
+    }
+
+    // Update is called once per frames
+    void Update()
+    {
+        dx = Input.GetAxisRaw("Horizontal");
+        dy = Input.GetAxisRaw("Vertical");
+        Debug.Log(dx);
+        if (dx != 0 && dy != 0 )
+        {
+            return;
+        }
+        if (dx > 0)
+        {
+            if (directions.Count > 0 && directions.Peek() == 3)
+            {
+                //going backwards
+                Debug.Log("Going back");
+                goingBackwards = true;
+                directions.Pop();
+                Destroy(path[path.Count - 1].gameObject);
+                path.RemoveAt(path.Count - 1);
+            } else
+            {
+                directions.Push(1);
+                Debug.Log("Going forth");
+
+                goingBackwards = false;
+            }
+        }
+        if (dx < 0)
+        {
+            if (directions.Count > 0 && directions.Peek() == 1)
+            {
+                //going backwards
+                Debug.Log("Going back");
+                goingBackwards = true;
+                directions.Pop();
+                Destroy(path[path.Count - 1].gameObject);
+                path.RemoveAt(path.Count - 1);
+            } else
+            {
+                directions.Push(3);
+                Debug.Log("Going forth");
+
+                goingBackwards = false;
+
+            }
+        }
+        if (dy > 0)
+        {
+            if (directions.Count > 0 && directions.Peek() == 2)
+            {
+                //going backwards
+                Debug.Log("Going back");
+                goingBackwards = true;
+                directions.Pop();
+                Destroy(path[path.Count - 1].gameObject);
+                path.RemoveAt(path.Count - 1);
+            } else
+            {
+                directions.Push(0);
+                Debug.Log("Going forth");
+
+                goingBackwards = false; 
+            }
+        }
+        if (dy < 0)
+        {
+            if (directions.Count > 0 && directions.Peek() == 0)
+            {
+                //going backwards
+                Debug.Log("Going back");
+                goingBackwards = true;
+                directions.Pop();
+                Destroy(path[path.Count - 1].gameObject);
+                path.RemoveAt(path.Count - 1);
+            } else
+            {
+                directions.Push(2);
+                Debug.Log("Going forth");
+
+                goingBackwards = false;
+            }
+        }
+        dx = dx * speed;
+        dy = dy * speed;
+        circle.transform.Translate(dx, dy, 0.0f);
+
+        if ((dx != 0.0f || dy != 0.0f) && !goingBackwards)
+        {
+            Transform squarePart = Instantiate(square.transform, circle.transform.position, circle.transform.rotation);
+            squarePart.SetParent(PathParent.transform);
+            path.Add(squarePart);
+        }
+    }
+
+}
